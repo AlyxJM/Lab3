402da93c7c23ca73e87cdaeb2f8483de60112045
AlyxJM <merritt9@illinois.edu>
2018-09-18 12:30:34 -0500

    Added helper function provided. Created a total word count function and a function that counts the number of a specific word in the text.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,50 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String[] unused) {
-        System.out.println("Ran");
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
+
+    public static int totalWordCount (String websiteText) {
+        int totalNumWords = 0;
+        String arrayOfWords[] = websiteText.split(" ");
+        totalNumWords = arrayOfWords.length;
+        return totalNumWords;
+
+    }
+
+    public static int countOneWord (String websiteText, String inputWord) {
+        int numWord = 0;
+        String arrayOfWords[] = websiteText.split(" ");
+        for (int currentWord = 0; currentWord < arrayOfWords.length; currentWord++) {
+            if (arrayOfWords[currentWord].equalsIgnoreCase(inputWord)) {
+                numWord++;
+            }
+        }
+        return numWord;
+    }
+
+    public static void main(String[] unused) {
+        System.out.println(totalWordCount(urlToString("http://erdani.com/tdpl/hamlet.txt")));
+        System.out.println(countOneWord(urlToString("http://erdani.com/tdpl/hamlet.txt"), "Prince"));
+    }
+
+
+
 }
